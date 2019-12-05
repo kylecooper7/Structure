d7ea7ff9309fdbd24734f488bd864ccd281dd8c0
kyle.cooper <kyle.cooper@MU-C1-15.mullen.pvt.k12.co.us>
2019-12-05 11:22:41 -0700

    kemr

---
+++ Structures/src/Structure.java
@@ -0,0 +1,12 @@
+
+public abstract class Structure
+	{
+protected int squareFootage;
+public Structure() {
+	
+}
+
+public void selfDestruct() {
+	System.out.println("Boom. Your structure has been nuked.");
+}
+	}
